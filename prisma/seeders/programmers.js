e0960548c2feebad271c1a360508cdad1433a2dc
Karin Hogenbirk <karinhogenbirk93@gmail.com>
2022-11-03 10:01:44 +0100

    Updated build script

--- prisma/seeders/programmers.js
+++ prisma/seeders/programmers.js
@@ -1,29 +1,15 @@
 // database
-// const programmers = require("../../programmers.json");
+const programmers = require("../../programmers.json");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 // database vullen
-// async function main() {
-//   const programmer = await prisma.programmer.create({
-//     data: programmers,
-//   });
-//   console.log(programmer);
-// }
-
-//test update
-// async function main() {
-//   const updateProgrammer = await prisma.programmer.update({
-//     where: { name: "Karin Hogenbirk" },
-//     data: { vote: 1 },
-//     // data: { vote: { increment: 1 } },
-//   });
-
-//   const programmer = await prisma.programmer.find({
-//     where: { name: "Karin Hogenbirk" },
-//   });
-//   console.log(programmer);
-// }
+async function main() {
+  const programmer = await prisma.programmer.create({
+    data: programmers,
+  });
+  console.log(programmer);
+}
 
 main()
   .then(async () => {
