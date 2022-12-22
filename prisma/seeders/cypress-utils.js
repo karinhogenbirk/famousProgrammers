7b6d282d70b732e561d1679876ccccafd090dda3
Karin Hogenbirk <karinhogenbirk93@gmail.com>
2022-12-22 11:28:49 +0100

    Added user data

--- prisma/seeders/cypress-utils.js
+++ prisma/seeders/cypress-utils.js
@@ -8,6 +8,13 @@ async function seeddb() {
   const programmer = await prisma.programmer.createMany({
     data: programmers,
   });
+
+  const newUser = await prisma.user.upsert({
+    data: {
+      email: "karinhogenbirk93@gmail.com",
+      password: "$2b$10$qm3mb3KenoJFIwY55XqcTOUl8xlXZDA0OcIPwVM8uC.Tk72ICcwmO",
+    },
+  });
   return null;
 }
 
